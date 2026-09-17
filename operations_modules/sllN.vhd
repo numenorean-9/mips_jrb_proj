c4448c7efff60816206c581e5fbe7997f6f3ccb7
numenorean <jbaker@exit20.com>
2026-09-17 19:57:40 -0400

    Initial commit of logical left shift operation

---
+++ operations_modules/sllN.vhd
@@ -0,0 +1,33 @@
+----------------------------------------------------------------------------------
+-- Company: FUN!
+-- Engineer: Jackson Baker
+-- 
+-- Create Date: 01/13/2022 10:19:24 AM
+-- Design Name: 
+-- Module Name: sllN- behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: Logical left shift for a generic N bit input, shifted by an M bit input
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sllN is
+    generic()
+    port()
+end sllN;
+
+architecture behavioral of sllN is 
+    type shifty_array is array 
+
+
+begin
